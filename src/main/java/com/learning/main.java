4e08269a1cefb92ddbfe49632015841bae582f19
saritapal-123 <sarita.pal6448@gmail.com>
2026-07-15 22:58:03 +0530

    update

--- src/main/java/com/learning/main.java
+++ src/main/java/com/learning/main.java
@@ -15,11 +15,11 @@ public class main {
 		Employee bean = ioc.getBean("emp1", Employee.class);
 		System.out.println(bean);
 		
-	
+	/*
 		Employee bean2 = ioc.getBean("emp2", Employee.class);
 		System.out.println(bean2);
 		
-		
+		*/
 		
 		
 	}
--- src/main/java/com/learning/model/Employee.java
+++ src/main/java/com/learning/model/Employee.java
@@ -1,27 +1,72 @@
 package com.learning.model;
 
-import java.util.List;
-
-import java.util.Map;
-import java.util.Set;
-import lombok.NoArgsConstructor;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-@NoArgsConstructor
-@AllArgsConstructor
-@Data
 public class Employee {
 	private int id;
 	private String name, gender;
 	private int salary;
 	private Address address;
 	
+	public Employee() {
+		System.out.println("Employee.Employee()");
+	}
 	
-	private List<Integer> list;
-	private Set<Integer> set;
-	private Map<Integer, String> map;
+	public void setName(String name) {
+		this.name = name;
+		System.out.println("Employee.setName()");
+		
+	}
+
+	public Employee(int id, String name, String gender, int salary, Address address) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.gender = gender;
+		this.salary = salary;
+		this.address = address;
+		System.out.println("Employee.Employee(5)");
+	}
 	
-	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+	    this.id = id;
+	    System.out.println("Employee.setId()");
+	}
+
+	public String getGender() {
+	    return gender;
+	}
+
+	public void setGender(String gender) {
+	    this.gender = gender;
+	    System.out.println("Employee.setGender()");
+	}
+
+	public int getSalary() {
+	    return salary;
+	}
+
+	public void setSalary(int salary) {
+	    this.salary = salary;
+	    System.out.println("Employee.setSalary");
+	}
+
+	public Address getAddress() {
+	    return address;
+	}
+
+	public void setAddress(Address address) {
+	    this.address = address;
+	    System.out.println("Employee.setAddress()");
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", gender=" + gender + ", salary=" + salary + ", address="
+				+ address + "]";
+	}
+
 	
 		
 	
