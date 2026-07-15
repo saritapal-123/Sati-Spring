9b9f9de206afbda52cc2cbf3eb0829223e6e3763
saritapal-123 <sarita.pal6448@gmail.com>
2026-07-15 23:46:55 +0530

    update

--- src/main/java/com/learning/main.java
+++ src/main/java/com/learning/main.java
@@ -8,19 +8,16 @@ public class main {
 	
 	public static void main(String[] args) {
 		
-		ApplicationContext  ioc  = new ClassPathXmlApplicationContext("application-context.xml");
+		ApplicationContext  ioc  = new ClassPathXmlApplicationContext("inner.bean.xml");
 		
 		System.out.println("<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>");
 
 		Employee bean = ioc.getBean("emp1", Employee.class);
 		System.out.println(bean);
 		
-	/*
-		Employee bean2 = ioc.getBean("emp2", Employee.class);
-		System.out.println(bean2);
 		
-		*/
 		
+	
 		
 	}
 
